f399e253a8abdbc32353515da648e7a4510d3874
Michal Vasinek <michal.vasinek@vsb.cz>
2025-03-07 13:38:29 +0100

    testovaci projekt, pridani testovaci casu

---
+++ CSharpChecker/UnitTests/DiagramLoading.cs
@@ -0,0 +1,53 @@
+using System.Xml.Linq;
+
+using BPMNModel.Camunda;
+using BPMNModel.Model;
+using BPMNModel.XMLElements;
+using BPMNModel.XMLParser;
+
+using Serilog;
+using Serilog.Events;
+using Serilog.Formatting.Json;
+using Serilog.Sinks.File;
+using Utility;
+
+namespace UnitTests;
+
+[TestClass]
+public class DiagramLoading
+{
+    [TestMethod]
+    public void LoadWithNoDefinition()
+    {
+        Log.Logger = new LoggerConfiguration()
+                           // add console as logging target
+                           .WriteTo.Console(restrictedToMinimumLevel: LogEventLevel.Debug)
+                           // add a logging target for warnings and higher severity  logs
+                           // structured in JSON format
+                           .WriteTo.File(new JsonFormatter(), "important.json", LogEventLevel.Warning)
+                           // add a rolling file for all logs
+                           .WriteTo.File("all.logs",
+                                         restrictedToMinimumLevel: LogEventLevel.Verbose,
+                                         rollingInterval: RollingInterval.Day)
+                           .MinimumLevel.Verbose()
+            .CreateLogger();
+
+        var generator = Generator.CreateGenerator(Log.Logger);
+
+        CamundaExtensions.EnrichGenerator(Log.Logger, generator, true);
+
+        XDocument doc = ResourcesUtility.LoadResourceAsXDocument($@"diagrams/DiagramLoading/invalid.bpmn");
+
+        var parser = XmlParser.Parse(Log.Logger, generator, doc);
+
+        if (parser.HasErrors)
+        {
+            foreach (var error in parser.Errors)
+            {
+                Log.Error(error);
+            }
+        }
+
+        var model = Factory.ProcessModel(Log.Logger, parser);
+    }
+}
